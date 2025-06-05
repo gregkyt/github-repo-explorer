ddb23b3191c12e8c3f207433c3658c300dc7bbf3
Bangkit Purbandoro <gregkyt@gmail.com>
2025-06-05 10:55:21 +0700

    chore: setup gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "github-repo-explorer";
 const nextConfig = {
+  output: "export",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
   env: {
-    BASE_URL: process.env.BASE_URL,
-    SERVICE_BASE_URL: process.env.SERVICE_BASE_URL,
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    BASE_URL: isProd ? process.env.NEXT_PUBLIC_BASE_URL : process.env.BASE_URL,
+    SERVICE_BASE_URL: isProd
+      ? process.env.NEXT_PUBLIC_SERVICE_BASE_URL
+      : process.env.SERVICE_BASE_URL,
+    GITHUB_CLIENT_ID: isProd
+      ? process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID
+      : process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: isProd
+      ? process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET
+      : process.env.GITHUB_CLIENT_SECRET,
   },
   reactStrictMode: false,
 };
