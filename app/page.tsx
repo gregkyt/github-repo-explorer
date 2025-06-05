ed47bccd3fd99eb9ee49f9b882bc2d4e00290028
Bangkit Purbandoro <gregkyt@gmail.com>
2025-06-05 13:59:28 +0700

    refactor: delete github login

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,16 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
-import { FaGithub } from "react-icons/fa";
+import List from "@/components/list";
+import Search from "@/components/search";
+import { Suspense } from "react";
 
-const GitHub = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const clientId = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
-  const state = process.env.NEXT_PUBLIC_GITHUB_STATE;
-
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-
-  useEffect(() => {
-    if (token) onSuccessLogin(token);
-  }, [token]);
-
-  async function onSuccessLogin(token: string) {
-    router.push(`/dashboard?token=${token}`);
-  }
-
-  function goToGitHubLogin() {
-    setIsLoading(true);
-    router.push(
-      `https://github.com/login/oauth/authorize?client_id=${clientId}&state=${state}`
-    );
-  }
-
+export default function Dashboard() {
   return (
-    <button
-      className={`flex btn ${isLoading ? "loading loading-dots" : ""}`}
-      onClick={goToGitHubLogin}
-    >
-      <label>Login with GitHub</label>
-      <FaGithub />
-    </button>
-  );
-};
-
-export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="p-4">
       <Suspense fallback={<div>Loading...</div>}>
-        <GitHub />
+        <Search />
       </Suspense>
+      <List />
     </div>
   );
 }
