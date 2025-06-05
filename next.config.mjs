3d441f50b2701c06083cf05c73b8ba48faf21008
Bangkit Purbandoro <gregkyt@gmail.com>
2025-06-05 09:53:21 +0700

    chore: library and setup project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    SERVICE_BASE_URL: process.env.SERVICE_BASE_URL,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+  },
+  reactStrictMode: false,
+};
 
 export default nextConfig;
---
+++ plugin/axios.ts
@@ -0,0 +1,28 @@
+// "use client";
+
+import axios from "axios";
+// import { cookies } from "next/headers";
+
+const baseURL = process.env.SERVICE_BASE_URL;
+const api = axios.create({
+  baseURL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/vnd.github+json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use((response) => {
+  return response;
+});
+
+export { api };
